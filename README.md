a730853b2524d8c91d349a33ca4fd7d6a9985f95
StaciAF <aaen.it19@gmail.com>
2020-09-26 23:10:41 +0000

    adds task 3 file updates, circle in place, could not get API connection

--- README.md
+++ README.md
@@ -158,6 +158,8 @@ Alexa Orrico - [Github](https://github.com/alexaorrico) / [Twitter](https://twit
 Jennifer Huang - [Github](https://github.com/jhuang10123) / [Twitter](https://twitter.com/earthtojhuang)  
 Jhoan Zamora - [Github](https://github.com/jzamora5) / [Twitter](https://twitter.com/JhoanZamora10)  
 David Ovalle - [Github](https://github.com/Nukemenonai) / [Twitter](https://twitter.com/disartDave)
+Lance Burklin - [Github](https://github.com/kancewburklin)
+Staci Aaenson-Fletcher - [Github](https://github.com/StaciAF)
 
 Second part of Airbnb: Joann Vuong
 ## License
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const $ = window.$;
+document.addEventListener('DOMContentLoaded', function () {
+  const items = [];
+  const names = [];
+  $('input:checkbox').change(function () {
+    const theName = $(this).attr('data-name');
+    const theId = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
+      items.push(theId);
+      names.push(theName);
+      const namesNice = names.join(', ');
+      $('.amenities h4').text(namesNice);
+    } else {
+      const indexOne = items.indexOf(theId);
+      items.splice(indexOne, 1);
+      const indexTwo = names.indexOf(theName);
+      names.splice(indexTwo, 1);
+      const namesNice = names.join(', ');
+      $('.amenities h4').text(namesNice);
+    }
+  });
+});
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (status === 'success') {
+	$.('#api_status').addClass('available');
+    } else {
+	$.('#api_status').removeClass('available');
+    }
+});
